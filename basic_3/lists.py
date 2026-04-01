267a36d51218b3b0daea1c2909f3c6f67c333c0e
alvmantura23 <alvmantura23@gmail.com>
2026-03-31 23:54:02 -0500

    Agregando lists.py

---
+++ basic_3/lists.py
@@ -0,0 +1,20 @@
+#----------------
+# Lists in Python
+#----------------
+
+#Characteristics of lists:
+# Las listas son estructuras de datos que nos permiten guardar mas de un valor en una sola variable
+# Son altamente flexibles, ya que pueden contener diferentes tipos de datos (numeros, cadenas, booleanos, listas, etc)
+# Se pueden modificar luego de haberse creado
+
+list_of_numbers = [1,2,3,4,5]
+ingredients_of_a_cake = ["huevo", "leche", "mantequilla", "harina"]
+empty_list = []
+
+data_list = ["Alvaro", "Cueva", 25, 1.66, True]
+
+print(list_of_numbers)
+print(ingredients_of_a_cake)
+print(empty_list)
+print(data_list)
+
